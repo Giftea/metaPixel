6af526a57d4383ebd46a3dc5b129f5b7ce47861c
Vijeth (GitHub Desktop) <38877972+vijethx@users.noreply.github.com>
2022-11-08 21:47:04 +0530

    app works only on polygon mumbai

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -8,41 +8,40 @@ import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider } = configureChains(
-  [
-    chain.polygonMumbai,
-    chain.mainnet,
-    chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-  ],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+	[chain.polygonMumbai],
+	[
+		jsonRpcProvider({
+			rpc: (chain) => ({
+				http: "https://rpc-mumbai.matic.today",
+			}),
+		}),
+	]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  chains,
+	appName: "Pixeed",
+	chains,
 });
 
 const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
+	autoConnect: true,
+	connectors,
+	provider,
 });
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        coolMode
-        chains={chains}
-        theme={midnightTheme({ accentColor: "#9932cc" })}
-      >
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider
+				coolMode
+				chains={chains}
+				theme={midnightTheme({ accentColor: "#9932cc" })}>
+				<ChakraProvider>
+					<Component {...pageProps} />
+				</ChakraProvider>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
 }
 
 export default MyApp;
