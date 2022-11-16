28e30bad6fb13d3dea48a6f8d0d722563e0d0323
Marybeee <maria.behappy.mb@gmail.com>
2022-11-16 16:31:07 +0100

    changed withdrawFundingPixeed to all PIXEED_TEAM members

--- contracts/contracts/RewardCreator.sol
+++ contracts/contracts/RewardCreator.sol
@@ -4,19 +4,21 @@ pragma solidity ^0.8.9;
 //import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControlEnumerable} from "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract RewardCreator is AccessControl {
+contract RewardCreator is AccessControlEnumerable {
     bytes32 public constant PIXEED_TEAM = keccak256("PIXEED_TEAM");
 
     using SafeERC20 for ERC20;
 
     ERC20 public immutable usdc;
 
+    mapping (address => uint) public claimExpiryTimestampPerAddress;
+
     event RewardedCreator (address creatorAddress, uint amountToCreator);
     event ContractRewarded (address pixeedContractAddress, uint amountToPixeed);
 
-    event FundingForPixeedWithdrawed(address withdrawAddress, uint withdrawAmount);
+    event FundingForPixeedWithdrawed(uint roleMemberCount, uint withdrawAmount);
 
     constructor (
         address usdcAddress
@@ -26,23 +28,44 @@ contract RewardCreator is AccessControl {
         usdc = ERC20(usdcAddress);
     }
 
-    function rewardCreator(uint valueToCreator, uint percentageToPixeed, address creatorAddress) external {
+    // modifier expiryTimestamp(address _pixeedWithdrawer) {
+    //     require(claimExpiryTimestampPerAddress[_pixeedWithdrawer] > block.timestamp, 
+    //     "Expiry date not over"
+    //     );       
+    //     _;
+    // }
+
+    function rewardCreator(uint valueToCreator, uint valueToPixeed, address creatorAddress) external {
         // safe transfer from msg.sender to creator with amount valueToCreator
         usdc.safeTransfer(creatorAddress, valueToCreator);
         emit RewardedCreator(creatorAddress, valueToCreator);
         // safe transfer from msg.snder to contractMatapixel with amount percentageToPixeed
-        usdc.transferFrom(msg.sender, address(this), percentageToPixeed);
-        emit ContractRewarded (address(this), percentageToPixeed);
+        usdc.transferFrom(msg.sender, address(this), valueToPixeed);
+        emit ContractRewarded (address(this), valueToPixeed);
     }
 
     // token to withdraw IERC20?
     function withdrawFundingPixeed() external {
         require(hasRole(PIXEED_TEAM, msg.sender));
-        // Transfer USDC tokens to the users wallet
-        uint withdrawAmount = address(this).balance;
-        usdc.transfer(msg.sender, withdrawAmount);
-        emit FundingForPixeedWithdrawed(msg.sender, withdrawAmount);
+        // calculate withdraw amount based on the number of pixeed members
+         // Transfer USDC tokens to the users wallet
+        uint roleMemberCount = getRoleMemberCount(PIXEED_TEAM);
+        //setClaimTimerForWalletAddress(msg.sender);
+        uint withdrawAmount = address(this).balance / roleMemberCount;
+        // send amount to all addresses of PIXEED_TEAM
+        for (uint i = 0; i < roleMemberCount; i++) {
+             usdc.transfer(getRoleMember(PIXEED_TEAM, i), withdrawAmount);
+        }
+        // transfer amount to all team members
+        emit FundingForPixeedWithdrawed(getRoleMemberCount(PIXEED_TEAM), withdrawAmount);
     }
+    // number of team member has to be set
+
+    // set limit during a month to claim for each address
+    // function setClaimTimerForWalletAddress(address pixeedWithdrawer) internal expiryTimestamp(pixeedWithdrawer) {
+    //     claimExpiryTimestampPerAddress[pixeedWithdrawer] = block.timestamp + 30 days;
+    // } 
+
 
     // function to show balance in this contract
     function getBalance() public view returns (uint) {
