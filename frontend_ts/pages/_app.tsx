e20f5a4fd46c03db648ac1a12596cbbfc23a3603
Giftea <eleojogiftuhiene@gmail.com>
2022-11-22 00:52:33 +0100

    Added ts frontend folder

---
+++ frontend_ts/pages/_app.tsx
@@ -0,0 +1,51 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { midnightTheme } from "@rainbow-me/rainbowkit";
+import { ChakraProvider } from "@chakra-ui/react";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+// import { AuthProvider } from "../context/AuthProvider";
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: "https://rpc-mumbai.matic.today",
+      }),
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Pixeed",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    // <AuthProvider>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        coolMode
+        chains={chains}
+        theme={midnightTheme({ accentColor: "#9932cc" })}
+      >
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+    /*  </AuthProvider>*/
+  );
+}
+
+export default MyApp;
