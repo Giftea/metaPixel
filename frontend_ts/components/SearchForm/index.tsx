37b6bd18d6a484c6ce4fe097638cd271c605c896
Marybeee <maria.behappy.mb@gmail.com>
2022-12-10 15:47:34 +0100

    Background change hero

--- frontend_ts/components/SearchForm/index.tsx
+++ frontend_ts/components/SearchForm/index.tsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { FormControl, Input, Stack } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import React from 'react'
+import { FormControl, Input, Stack } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
 
 const SearchForm = () => {
   return (
     <FormControl as="form" maxW="40rem" mx="auto">
-      <Stack borderRadius={'8px'} px="4" bg="#ffffff14" alignItems="center" direction="row">
+      <Stack
+        borderRadius={'8px'}
+        px="4"
+        bg="#ffffff14"
+        alignItems="center"
+        direction="row"
+      >
         <Input
           bg="none"
           color="#ffffff"
@@ -17,7 +23,7 @@ const SearchForm = () => {
         <SearchIcon color={'#FFFFFF99'} />
       </Stack>
     </FormControl>
-  );
-};
+  )
+}
 
-export default SearchForm;
+export default SearchForm
--- frontend_ts/data.ts
+++ frontend_ts/data.ts
@@ -1,15 +1,15 @@
 export const heroText = {
-  title: "A decentralised photo sharing platform to empower photographers",
-  subtitle: "",
-};
+  title: 'A decentralized photo sharing platform to empower photographers',
+  subtitle: 'We empower photographers to be valued for their content',
+}
 
 export const dummyPhotos = [
-  "/images/dummyPhotos/1.jpg",
-  "/images/dummyPhotos/2.jpg",
-  "/images/dummyPhotos/3.jpg",
-  "/images/dummyPhotos/8.jpg",
-  "/images/dummyPhotos/4.jpg",
-  "/images/dummyPhotos/6.jpg",
-  "/images/dummyPhotos/5.jpg",
-  "/images/dummyPhotos/7.jpg",
-];
+  '/images/dummyPhotos/1.jpg',
+  '/images/dummyPhotos/2.jpg',
+  '/images/dummyPhotos/3.jpg',
+  '/images/dummyPhotos/8.jpg',
+  '/images/dummyPhotos/4.jpg',
+  '/images/dummyPhotos/6.jpg',
+  '/images/dummyPhotos/5.jpg',
+  '/images/dummyPhotos/7.jpg',
+]
