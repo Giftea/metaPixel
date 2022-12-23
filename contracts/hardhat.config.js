886d4009fd5706778dc0d69bba1389a3ca0f645b
Marybeee <maria.behappy.mb@gmail.com>
2022-12-23 08:00:47 +0100

    config add

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,18 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-  networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    mumbai: {
-      url: process.env.STAGING_INFURA_URL,
-      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
-      gas: 2100000,
-      gasPrice: 8000000000,
-    },
-  },
+  defaultNetwork: "hardhat",
+
+  // networks: {
+  //   hardhat: {},
+  //   mumbai: {
+  //     url: process.env.STAGING_INFURA_URL,
+  //     accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+  //     gas: 2100000,
+  //     gasPrice: 8000000000,
+  //   },
+  // },
 };
